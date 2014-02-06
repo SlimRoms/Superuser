8b29039b1ca1d557e5220caa2af25512afcf1d70
David Vaz Guijarro <vazdavid@icloud.com>
2014-02-06 01:19:38 +0100

    Delete time wait [su] + copyright

--- Superuser/src/com/koushikdutta/superuser/MultitaskSuRequestActivity.java
+++ Superuser/src/com/koushikdutta/superuser/MultitaskSuRequestActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 Koushik Dutta (@koush)
+ * Copyright (C) 2014 Slim+ Team Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +64,7 @@ public class MultitaskSuRequestActivity extends FragmentActivity {
     
     Handler mHandler = new Handler();
     
-    int mTimeLeft = 3;
+    int mTimeLeft = 0;
     
     Button mAllow;
     Button mDeny;
